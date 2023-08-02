084cf813d437a6fe405724e01b4cdadb33539661
shoaibhasann <shoaibhasan0940@gmail.com>
2023-08-02 06:17:07 +0530

    Create alert component

---
+++ src/components/Alert.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function Alert({message, type}) {
+    const alertStyle =
+      type === ""
+        ? "flex items-center bg-red-100 max-w-[200px] rounded-lg p-4 mb-4 text-lg text-slate-950"
+        : "flex items-center bg-green-100 max-w-[200px] rounded-lg p-4 mb-4 text-lg text-slate-950";
+  return (
+    <div
+      className={alertStyle}
+      role="alert"
+    >
+      <svg
+        class="w-5 h-5 inline mr-3"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+          clip-rule="evenodd"
+        ></path>
+      </svg>
+      <div>
+        <span className="font-medium">{message}</span>
+      </div>
+    </div>
+  );
+}
+
+export default Alert
